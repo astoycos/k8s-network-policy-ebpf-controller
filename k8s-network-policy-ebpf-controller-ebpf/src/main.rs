5d2b70fdc6bdb87d7f63106fa5146fdadfc7207d
Andrew Stoycos <astoycos@redhat.com>
2022-03-09 17:33:10 -0500

    Get Basic BPF XDP program running
    Signed-off-by: Andrew Stoycos <astoycos@redhat.com>

--- k8s-network-policy-ebpf-controller-ebpf/src/main.rs
+++ k8s-network-policy-ebpf-controller-ebpf/src/main.rs
@@ -3,23 +3,80 @@
 
 use aya_bpf::{
     bindings::xdp_action,
-    macros::xdp,
+    macros::{map, xdp},
+    maps::PerfEventArray,
     programs::XdpContext,
+    maps::HashMap,
 };
+
+use core::mem;
+use memoffset::offset_of;
+use k8s_network_policy_ebpf_controller_common::PacketLog;
+
+mod bindings;
+use bindings::{ethhdr, iphdr};
+
+#[map(name = "EVENTS")]
+static mut EVENTS: PerfEventArray<PacketLog> = PerfEventArray::<PacketLog>::with_max_entries(1024, 0);
+
+#[map(name = "BLOCKLIST")]
+static mut BLOCKLIST: HashMap<u32, u32> = HashMap::<u32, u32>::with_max_entries(1024, 0);
 
 #[xdp(name="k8s_network_policy_ebpf_controller")]
 pub fn k8s_network_policy_ebpf_controller(ctx: XdpContext) -> u32 {
-    match unsafe { try_k8s_network_policy_ebpf_controller(ctx) } {
+     match unsafe { try_k8s_network_policy_ebpf_controller(ctx) } {
         Ok(ret) => ret,
         Err(_) => xdp_action::XDP_ABORTED,
     }
 }
 
-unsafe fn try_k8s_network_policy_ebpf_controller(_ctx: XdpContext) -> Result<u32, u32> {
-    Ok(xdp_action::XDP_PASS)
+#[inline(always)]
+unsafe fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
+    let start = ctx.data();
+    let end = ctx.data_end();
+    let len = mem::size_of::<T>();
+
+    if start + offset + len > end {
+        return Err(());
+    }
+
+    Ok((start + offset) as *const T)
+}
+
+fn block_ip(address: u32) -> bool {
+    unsafe { BLOCKLIST.get(&address).is_some() }
+}
+
+unsafe fn try_k8s_network_policy_ebpf_controller(ctx: XdpContext) -> Result<u32, ()> {
+    let h_proto = u16::from_be(unsafe { *ptr_at(&ctx, offset_of!(ethhdr, h_proto))? });
+    if h_proto != ETH_P_IP {
+        return Ok(xdp_action::XDP_PASS);
+    }
+    let source = u32::from_be(unsafe { *ptr_at(&ctx, ETH_HDR_LEN + offset_of!(iphdr, saddr))? });
+
+    let action = if block_ip(source) {
+        xdp_action::XDP_DROP
+    } else {
+        xdp_action::XDP_PASS
+    };
+
+    let log_entry = PacketLog {
+        ipv4_address: source,
+        action: action,
+    };
+
+    if source == 168328460 {
+        unsafe {
+            EVENTS.output(&ctx, &log_entry, 0);
+        }
+    }
+    Ok(action)
 }
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     unsafe { core::hint::unreachable_unchecked() }
 }
+
+const ETH_P_IP: u16 = 0x0800;
+const ETH_HDR_LEN: usize = mem::size_of::<ethhdr>();
--- k8s-network-policy-ebpf-controller/src/main.rs
+++ k8s-network-policy-ebpf-controller/src/main.rs
@@ -1,46 +1,66 @@
-use aya::{include_bytes_aligned, Bpf};
 use anyhow::Context;
-use aya::programs::{Xdp, XdpFlags};
-use log::info;
-use simplelog::{ColorChoice, ConfigBuilder, LevelFilter, TermLogger, TerminalMode};
-use structopt::StructOpt;
-use tokio::signal;
+use aya::{
+    maps::perf::AsyncPerfEventArray,
+    programs::{Xdp, XdpFlags},
+    util::online_cpus,
+    Bpf,
+    maps::HashMap,
+};
+use bytes::BytesMut;
+use std::{
+    env, fs, net,
+};
+use tokio::{signal, task};
 
-#[derive(Debug, StructOpt)]
-struct Opt {
-    #[structopt(short, long, default_value = "eth0")]
-    iface: String,
-}
+use k8s_network_policy_ebpf_controller_common::PacketLog;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    let opt = Opt::from_args();
+    let path = match env::args().nth(1) {
+        Some(path) => path,
+        None => panic!("no path provided"),
+    };
+    let iface = match env::args().nth(2) {
+        Some(iface) => iface,
+        None => "eth0".to_string(),
+    };
 
-    TermLogger::init(
-        LevelFilter::Debug,
-        ConfigBuilder::new()
-            .set_target_level(LevelFilter::Error)
-            .set_location_level(LevelFilter::Error)
-            .build(),
-        TerminalMode::Mixed,
-        ColorChoice::Auto,
-    )?;
+    let data = fs::read(path)?;
+    let mut bpf = Bpf::load(&data)?;
 
-    // This will include your eBPF object file as raw bytes at compile-time and load it at
-    // runtime. This approach is recommended for most real-world use cases. If you would
-    // like to specify the eBPF program at runtime rather than at compile-time, you can
-    // reach for `Bpf::load_file` instead.
-    let mut bpf = Bpf::load(include_bytes_aligned!(
-        "../../target/bpfel-unknown-none/release/k8s-network-policy-ebpf-controller"
-    ))?;
     let program: &mut Xdp = bpf.program_mut("k8s_network_policy_ebpf_controller").unwrap().try_into()?;
     program.load()?;
-    program.attach(&opt.iface, XdpFlags::default())
+    program.attach(&iface, XdpFlags::default())
         .context("failed to attach the XDP program with default flags - try changing XdpFlags::default() to XdpFlags::SKB_MODE")?;
 
-    info!("Waiting for Ctrl-C...");
-    signal::ctrl_c().await?;
-    info!("Exiting...");
+    let mut blocklist: HashMap<_, u32, u32> = HashMap::try_from(bpf.map_mut("BLOCKLIST")?)?;
+    let block_addr : u32 = net::Ipv4Addr::new(10, 8, 125, 12).try_into()?;
+    blocklist.insert(block_addr, 0, 0)?;
 
-    Ok(())
+    let mut perf_array = AsyncPerfEventArray::try_from(bpf.map_mut("EVENTS")?)?;
+    
+    for cpu_id in online_cpus()? {
+        let mut buf = perf_array.open(cpu_id, None)?;
+
+        task::spawn(async move {
+            let mut buffers = (0..10)
+                .map(|_| BytesMut::with_capacity(1024))
+                .collect::<Vec<_>>();
+
+            loop {
+                let events = buf.read_events(&mut buffers).await.unwrap();
+                for i in 0..events.read {
+                    let buf = &mut buffers[i];
+                    let ptr = buf.as_ptr() as *const PacketLog;
+                    let data = unsafe { ptr.read_unaligned() };
+                    let src_addr = net::Ipv4Addr::from(data.ipv4_address);
+                    println!("LOG: SRC {}, SRCRAW {}, ACTION {}", src_addr, data.ipv4_address, data.action);
+                }
+            }
+        });
+    }
+    
+    signal::ctrl_c().await.expect("failed to listen for event");
+    Ok::<_, anyhow::Error>(())
 }
+    
